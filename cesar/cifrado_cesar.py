c7b0f9c9355a91d17e34b0e953281cbe95d1d96b
Juanse616 <juansebastian.farmaplast@gmail.com>
2024-05-20 23:27:38 -0500

    cifrado cesar

---
+++ cesar/cifrado_cesar.py
@@ -0,0 +1,36 @@
+import string
+from langdetect import detect
+
+ALFABETO = string.ascii_lowercase
+
+def algoritmo_descifrado(texto_cifrado, clave_descifrado):
+    """ descifra el texto cifrado a partir de la clave de descifrado"""
+
+    texto_plano = ""
+    for letra in texto_cifrado:
+        if letra not in ALFABETO:
+            texto_plano += letra
+        else:
+            indice_letra_cifrada = ALFABETO.index(letra)
+            indice_letra_descifrada = indice_letra_cifrada - clave_descifrado
+
+            texto_plano += ALFABETO[indice_letra_descifrada]
+    return texto_plano
+
+def fuerza_bruta(texto_cifrado):
+    """ esta funcioin realiza fuerza bruta sobre eñ texto cifrado"""
+    espacio_claves = range(len(ALFABETO))
+    for clave in espacio_claves:
+        texto_plano = algoritmo_descifrado(texto_cifrado, clave)
+        lenguaje = detect(texto_plano)
+        if lenguaje == "es":
+            print(f"el texto decifrado es: {texto_plano}")
+            print(f"la clave de descifrado es: {clave}")
+            return
+
+if __name__ == "__main__":
+
+    texto_cifrado = input("introducir el texto cifrado: ").lower()
+
+
+    fuerza_bruta(texto_cifrado)
